aac9edd533020ba618b628f4a6bc64c0391ef777
lanchingn <88715939+lanchingn@users.noreply.github.com>
2021-08-10 17:58:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'bbi của em àaa ',
+    introDesc: `trái đất vốn lạ thường
+    mà sao em cứ đi nhầm đường
+    lạc vào tim anh lẻ loi
+    đằng sau chữ yêu đây là thương`,
+    btnIntro: 'kakaka mì gók nhưng yêu anh',
+    title: 'phải chăng em đã yêu ngay từ cái nhìn đầu tiên',
+    desc: 'phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'yêu anh nhất quả đất',
+    btnNo: 'vẫn là yêu anh siêu nhiều ',
+    question:'trên thế giới hơn 7 tỉ người mà sao em lại yêu anh',
+    btnReply: 'love uu bbi',
+    reply: 'yêu thì yêu không yêu buộc yêu ',
+    mess: 'anh yêu em',
+    messDesc: 'hết dịch rồi mình đi date nhó bbi.',
+    btnAccept: 'oki bbi',
 }
